4887f41c8966e74613e99fd192eb3c3803ff225b
Daniil <porvipop@mail.ru>
2022-08-20 18:34:42 +0300

    Create Use cases: GetAllHeroes, SearchHeroes, GetSelectedHero

---
+++ app/src/main/java/com/example/astonlolapp/domain/use_cases/get_all_heroes/GetAllHeroesUseCase.kt
@@ -0,0 +1,14 @@
+package com.example.astonlolapp.domain.use_cases.get_all_heroes
+
+import androidx.paging.PagingData
+import com.example.astonlolapp.data.repository.Repository
+import com.example.astonlolapp.domain.model.Hero
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetAllHeroesUseCase @Inject constructor(val repository: Repository) {
+
+    operator fun invoke(): Flow<PagingData<Hero>> {
+        return repository.getAllHeroes()
+    }
+}
---
+++ app/src/main/java/com/example/astonlolapp/domain/use_cases/get_selected_hero/GetSelectedHeroUseCase.kt
@@ -0,0 +1,12 @@
+package com.example.astonlolapp.domain.use_cases.get_selected_hero
+
+import com.example.astonlolapp.data.repository.Repository
+import com.example.astonlolapp.domain.model.Hero
+
+class GetSelectedHeroUseCase(private val repository: Repository) {
+
+    suspend operator fun invoke(heroId: Int): Hero {
+        return repository.getSelectedHero(heroId = heroId)
+    }
+
+}
